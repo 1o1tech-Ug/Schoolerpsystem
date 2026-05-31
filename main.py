21f8443786ab5d02a7ba5f3d97e8320d63572738
1o1tech-Ug <1o1tech9999@gmail.com>
2026-06-01 02:19:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from app import create_app
-
+import traceback
 app = create_app()
 
+
+@app.errorhandler(500)
+def internal_error(e):
+    return {"error": str(e), "trace": traceback.format_exc()}, 500
 if __name__ == "__main__":
     app.run(debug=False)#change to false in production
